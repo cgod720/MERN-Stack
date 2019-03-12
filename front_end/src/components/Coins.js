88ba5a952c63412ce5e20d0ce6f4a954c70d29b9
Carlos Godoy <cgod720@gmail.com>
2019-03-12 13:15:45 -0400

    four spans showing per coin

--- front_end/src/components/Coins.js
+++ front_end/src/components/Coins.js
@@ -6,7 +6,9 @@ class Coins extends Component {
     return(
       <div className="coin">
         <span>{this.props.coin.name}  $</span>
-        <span>{this.props.coin.quote.USD.price}</span>
+        <span>Price: {this.props.coin.quote.USD.price} </span>
+        <span>24h Change: {this.props.coin.quote.USD.percent_change_24h}%   </span>
+        <span>24h Volume: {this.props.coin.quote.USD.volume_24h}</span>
       </div>
     )
   }
