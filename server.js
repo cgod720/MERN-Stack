82ad67792a47ed29a9e078e10fade8e06e1e2c64
Carlos Godoy <cgod720@gmail.com>
2019-03-19 19:15:50 -0400

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const session =  require('express-session')
 const cors = require('cors');
 const rp = require('request-promise');
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 //Database
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/crypto_sphere'
